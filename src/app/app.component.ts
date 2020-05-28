20d861e749767fec932f46d060bd2aedfd981497
Iribarrem <Resource.Darlan.Iribarrem@br.experian.com>
2020-05-28 13:00:15 -0300

    fadein ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
   animations: [
+    trigger('list', [
+      state('initial',
+      style({ opacity: 0,  transition: 'opacity 1s ease', border: '1px solid red' }),
+      ),
+      state('final',
+      style({ opacity: 1, transition: 'opacity 1s ease', border: '1px solid green' }),
+    )
+  ]),
     trigger('openFilters', [
       state('initial', style({
         width: '100%',
@@ -26,7 +34,8 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
       transition('initial=>final', animate('0ms')),
       transition('final=>initial', animate('0ms'))
     ]),
-  ]
+
+  ],
 })
 export class AppComponent {
   title = 'expanded-menu';
@@ -36,9 +45,9 @@ export class AppComponent {
 
   currentState = 'initial';
 
-change() {
-  this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
-  this.open = !this.open;
-}
+  change() {
+    this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
+    this.open = !this.open;
+  }
 
 }
